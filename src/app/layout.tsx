1c7ae21d5af9d9707ae423e77f015b0713477ef8
sergio <moyano.serrano@gmail.com>
2023-09-26 21:48:04 +0200

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" data-theme='dark'>
+    <html lang="en" data-theme='cupcake'>
       <body className={inter.className}>{children}</body>
     </html>
   )
